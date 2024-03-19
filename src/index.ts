54be47082367ac7952eabee4865e3965ee375481
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2024-03-19 16:21:07 -0400

    Initializes boilerplate code, deps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-(() => {
-  console.log('hello-world');
-})();
+import { LitElement, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('homage-to-the-square')
+export class HomageToTheSquare extends LitElement {
+  @property()
+  sample = ''
+
+  render() {
+    return html`<p>Welcome to ${this.sample}</p>`;
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
       name: 'index',
       formats: ['es']
     },
-    outDir: './'
+    outDir: './dist'
   }
 });
